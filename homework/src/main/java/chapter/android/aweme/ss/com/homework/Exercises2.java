99c212eaa41a9af65c8a824fef0c9139336f1bef
PeterLong0612 <peterlong0612@outlook.com>
2019-01-29 21:52:56 +0800

    black backgroud

--- homework/src/main/java/chapter/android/aweme/ss/com/homework/Exercises2.java
+++ homework/src/main/java/chapter/android/aweme/ss/com/homework/Exercises2.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * 作业2：一个抖音笔试题：统计页面所有view的个数
@@ -20,6 +21,18 @@ public class Exercises2 extends AppCompatActivity {
 
     public int getAllChildViewCount(View view) {
         //todo 补全你的代码
-        return 0;
+        int i=0;
+        if (view != null) {
+            int count = 0;
+            if (!(view instanceof ViewGroup)) {
+                return 1;
+            }
+            ViewGroup group = (ViewGroup) view;
+            for (; i <group.getChildCount(); i++) {
+                count += getAllChildViewCount(group.getChildAt(i));
+            }
+            return count;
+        }
+        throw new RuntimeException("The view isn't null.");
     }
 }
