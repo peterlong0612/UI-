f504e460652529af7683aaf934216ded5e116807
PeterLong0612 <peterlong0612@outlook.com>
2019-01-29 21:46:08 +0800

    ajust

--- homework/src/main/java/chapter/android/aweme/ss/com/homework/Exercises3.java
+++ homework/src/main/java/chapter/android/aweme/ss/com/homework/Exercises3.java
@@ -1,70 +1,58 @@
 package chapter.android.aweme.ss.com.homework;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.widget.LinearLayout;
-import android.widget.Toast;
-
-import java.io.InputStream;
-import java.util.List;
 
 import chapter.android.aweme.ss.com.homework.model.Message;
 import chapter.android.aweme.ss.com.homework.model.PullParser;
 
+import java.io.InputStream;
+import java.util.List;
+
 /**
  * 大作业:实现一个抖音消息页面,所需资源已放在res下面
  */
-public class Exercises3 extends AppCompatActivity implements MyAdapter.ListItemClickListener{
+public class Exercises3 extends AppCompatActivity implements  MyAdapter.ListItemClickListener {
 
-    private static final String TAG = "peterlong";
-    private static final int LIST_ITEMS = 50;
-
+    private List<Message> messages;
+    private static final String TAG = "Message Views";
     private MyAdapter mAdapter;
-    private RecyclerView mListView;
-
-    private Toast mToast;
-
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tips);
-
+        //获取需要展示的数据
         try{
             InputStream assetInput = getAssets().open("data.xml");
-            List<Message> messages = PullParser.pull2xml(assetInput);
-        }   catch (Exception exception){
-            exception.printStackTrace();;
+            messages = PullParser.pull2xml(assetInput);
+        }catch (Exception exception){
+            exception.printStackTrace();
         }
+        //定义recyclerview
+        RecyclerView recyclerView = findViewById(R.id.rv_list);
 
-
-        Message msg=new Message();
-        if(msg.getIcon().equals("TYPE_ROTOBT")){
-            int drawableId = R.drawable.session_robot;
-        }
-
-        RecyclerView recyclerView= findViewById(R.id.rv_list);
+        //设置Manager，即设置其样式
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(layoutManager);
-        mAdapter = new MyAdapter(LIST_ITEMS,this);
-        mListView.setAdapter(mAdapter);
 
+        recyclerView.setHasFixedSize(true);
+        //mAdapter初始化
+        mAdapter = new MyAdapter(messages,this);
+        recyclerView.setAdapter(mAdapter);
     }
 
     @Override
-    public void onListItemClick(int clickedItemIndex) {
-        Log.d(TAG, "onListItemClick: ");
-        if (mToast != null) {
-            mToast.cancel();
-        }
-        String toastMessage = "Item #" + clickedItemIndex + " clicked.";
-        mToast = Toast.makeText(this, toastMessage, Toast.LENGTH_LONG);
-
-        mToast.show();
+    public void onListItemClick(String chat_target) {
+//        System.out.println(chat_target);
+        Intent it = new Intent(this, ChatRoom.class);
+        it.putExtra("chat_target", chat_target);
+        startActivity(it);
     }
+
 }
--- homework/src/main/java/chapter/android/aweme/ss/com/homework/MyAdapter.java
+++ homework/src/main/java/chapter/android/aweme/ss/com/homework/MyAdapter.java
@@ -1,38 +1,36 @@
 package chapter.android.aweme.ss.com.homework;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.content.Context;
-import android.view.LayoutInflater;
 
-import java.util.TooManyListenersException;
+import java.util.ArrayList;
+import java.util.List;
 
-import chapter.android.aweme.ss.com.homework.R;
+import chapter.android.aweme.ss.com.homework.model.Message;
 
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
-    private static final String TAG = "MyAdapter";
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
 
-    private int mItems;
-
-    private static int viewHolderCount;
-
+    List<Message> mData;
+    //点击事件监听
     private final ListItemClickListener mOnClickListener;
 
-    public MyAdapter(int ListItems, ListItemClickListener listener) {
-        mItems = ListItems;
+    public MyAdapter(List<Message> data, ListItemClickListener listener){
+        mData = data;
+        if (mData == null)
+            mData = new ArrayList<>();
         mOnClickListener = listener;
-        viewHolderCount = 0;
     }
 
     @NonNull
     @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewtype) {
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         Context context = viewGroup.getContext();
         int layoutIdForListItem = R.layout.im_list_item;
         LayoutInflater inflater = LayoutInflater.from(context);
@@ -45,51 +43,82 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyAdapter.MyViewHolder myViewHolder, int position) {
-        Log.d(TAG, "onBindViewHolder: #" + position);
-        myViewHolder.bind(position);
+    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int position) {
+        Message message = mData.get(position);
+        myViewHolder.updateUI(message);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+
+        return mData.size();
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-
-        private final ImageView avatar;
-        private final ImageView notice;
-        private final TextView title;
-        private final TextView description;
-        private final TextView time;
-
-        public MyViewHolder(@NonNull View itemView) {
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        //title
+        private final TextView view_tv_title;
+        private final TextView view_tv_description;
+        private final TextView view_tv_time;
+        private final ImageView view_iv_avator;
+        private final ImageView view_robot_notice;
+        public MyViewHolder(@NonNull View itemView){
             super(itemView);
-
-            avatar = (ImageView) itemView.findViewById(R.id.iv_avatar);
-            notice = (ImageView) itemView.findViewById(R.id.robot_notice);
-            title = (TextView) itemView.findViewById(R.id.tv_title);
-            time = (TextView) itemView.findViewById(R.id.tv_time);
-            description = (TextView) itemView.findViewById(R.id.tv_description);
+            //名称，描述，时间
+            view_tv_title = (TextView) itemView.findViewById(R.id.tv_title);
+            view_tv_description = (TextView) itemView.findViewById(R.id.tv_description);
+            view_tv_time = (TextView) itemView.findViewById(R.id.tv_time);
+            //头像，是否官方
+            view_iv_avator = (ImageView) itemView.findViewById(R.id.iv_avatar);
+            view_robot_notice = (ImageView) itemView.findViewById(R.id.robot_notice);
             itemView.setOnClickListener(this);
         }
 
-        public void bind(int position) {
-            title.setText(String.valueOf(position));
-            time.setText(String.valueOf(position));
-            description.setText(String.valueOf(position));
+        public void updateUI(Message message){
+            //设置名称，描述，时间
+            view_tv_title.setText(message.getTitle());
+            view_tv_description.setText(message.getDescription());
+            view_tv_time.setText(message.getTime());
+            //设置头像
+            if(message.getIcon().equals("TYPE_ROBOT")) {
+                view_iv_avator.setImageResource(R.drawable.session_robot);
+            } else if(message.getIcon().equals("TYPE_GAME")){
+                view_iv_avator.setImageResource(R.drawable.icon_micro_game_comment);
+            } else if(message.getIcon().equals("TYPE_SYSTEM")){
+                view_iv_avator.setImageResource(R.drawable.session_system_notice);
+            } else if(message.getIcon().equals("TYPE_USER")){
+                view_iv_avator.setImageResource(R.drawable.icon_girl);
+            } else if(message.getIcon().equals("TYPE_STRANGER")){
+                view_iv_avator.setImageResource(R.drawable.session_stranger);
+            }
+
+            System.out.print("/设置是否官方账号");
+            System.out.print(message.isOfficial());
+
+
+//            view_robot_notice.setImageResource(R.drawable.im_icon_notice_official);
+            //设置是否官方账号
+
+            if(message.isOfficial() == true) {
+                view_robot_notice.setImageResource(R.drawable.im_icon_notice_official);
+                view_robot_notice.setVisibility(View.VISIBLE);
+
+
+
+
+            }
         }
 
         @Override
         public void onClick(View v) {
-            int clickedPosition = getAdapterPosition();
+            System.out.println();
             if (mOnClickListener != null) {
-                mOnClickListener.onListItemClick(clickedPosition);
+                mOnClickListener.onListItemClick(view_tv_title.getText().toString());
             }
         }
-    }
 
+    }
     public interface ListItemClickListener {
-        void onListItemClick(int clickedItemIndex);
+        void onListItemClick(String chat_target);
+
     }
 }
